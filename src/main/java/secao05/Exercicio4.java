fe38c6723f81328260f4dde30ea646b07c99ed82
Luziano Farias <luziano-farias1993@hotmail.com>
2023-04-25 01:27:33 -0300

    Atualizando Estrutura condicional

--- src/main/java/secao05/Exercicio4.java
+++ src/main/java/secao05/Exercicio4.java
@@ -6,5 +6,20 @@ public class Exercicio4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite a hora inicial e a hora final do jogo: ");
+        int horaIni = sc.nextInt();
+        int horaFinal = sc.nextInt();
+
+        int duracao;
+        if (horaIni < horaFinal){
+             duracao = horaFinal - horaIni;
+        }else {
+             duracao = 24 - horaIni + horaFinal;
+
+        }
+        System.out.println("O jogo durou " + duracao + " hora(s)");
+
+        sc.close();
     }
+
 }
